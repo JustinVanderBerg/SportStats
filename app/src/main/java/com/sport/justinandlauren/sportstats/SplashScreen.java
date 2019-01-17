e59a2f344f39df2143c8edc54749aed25ca24318
justin <vanderbergjustin@gmail.com>
2019-01-16 23:20:18 -0500

    Added splash screen

--- app/src/main/java/com/sport/justinandlauren/sportstats/SplashScreen.java
+++ app/src/main/java/com/sport/justinandlauren/sportstats/SplashScreen.java
@@ -1,6 +1,9 @@
 package com.sport.justinandlauren.sportstats;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,17 +16,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Context context = this;
+        //show app splash screen for 2 seconds
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void run() {
+                Intent intent = new Intent(context,LoadScreen.class);
+                startActivity(intent);
             }
-        });
+
+        },3000);//number of seconds to show splash screen for
     }
 
 }
